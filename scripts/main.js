d72bd26609fc4a3db716bcfccff62d1888cd98e3
ISM <ilichev.stas@gmail.com>
2018-03-29 16:00:26 +0300

    Add button (previous), add (autorotation)

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,4 @@
-// jQuery.fn.rotate = function(degrees, f) {
-//     var loc_deg=degrees-90;
-//     var steps= 12;
-//     var callback=f ||function () {};
-//     var that=this;
-//     var timer = setInterval(function () {
-//         loc_deg=loc_deg+7.5;
-//         that.css({'transform' : 'rotateY('+ loc_deg +'deg)'});
-//         steps--;
-//         if(steps===0){
-//             clearInterval(timer);
-//             callback();
-//         }
-//     }, 20);
-//     return $(this);
-// };
+
 // window.onload=function () {
 //     function makeDeg() {
 //         var currentDeg = 0;
@@ -67,20 +52,27 @@ function Rotator(obj_1){
   for(let i=0;i<obj_1["box"].length;i++){
     rotateEven[i]=1;
   }
-  var quant = $(".slider_box .slider_content").length / 2;
+  var quant=[];
+  for(let i=0;i<obj_1["box"].length;i++){
+    quant[i] = $(obj_1["box"][i]+" .slider_content").length;
+  }
   
   $(obj_1["button"]).on("click", function(e) {
     e.preventDefault();
     $(".button_box .button_disable").removeClass("no_display");
 //деактивировали кнопку и началось выполнение переворота
 
+/* Запускаем цикл вращения каждого контейнера в отдельности */
     for (let i=0; i<obj_1["box"].length;i++){
+      /* Вражаем на 90 градусов */
       $(obj_1["box"][i]).rotate(new_deg[i](), function() {
-        if (current_number[i] == quant) {
+        /* Если переворот был последний, то новый переворот будет показывать самый первый контент */
+        if (current_number[i] == quant[i]) {
           current_number[i] = 0;
         }
         current_number[i]++;
         rotateEven[i]++;
+        /* Тут переворачиваем контент после переворота потомушто при нечетном перевороте контент на обратной стороне блока (зеркальный) */
         if (rotateEven[i] % 2 == 0) {
           $(obj_1["box"][i]+" .slider_content[data-num=" + current_number[i] + "]").css({
             transform: "rotateY(180deg)"
@@ -90,12 +82,14 @@ function Rotator(obj_1){
             transform: "rotateY(360deg)"
           });
         }
+        /* скрываем старый контент и показываем новый (когда коробка повернута на 90 градусов, чтобы никто не увидел) */
         $(obj_1["box"][i]+" .yes_display")
           .addClass("no_display")
           .removeClass("yes_display");
         $(obj_1["box"][i]+" .slider_content[data-num=" + current_number[i] + "]")
           .addClass("yes_display")
           .removeClass("no_display");
+          /* Доворачиваем еще на 90 */
         $(obj_1["box"][i]).rotate(new_deg[i](), function() {
           $(".button_box .button_disable").addClass("no_display");
         });
@@ -129,46 +123,19 @@ user_object={
 window.onload=function(){
   var new_rotator=new Rotator(user_object);
 }
-/* window.onload = function() {
-  function makeDeg() {
-    var currentDeg = 0;
-
-    return function() {
-      return (currentDeg += 90);
-    };
-  }
-  var new_deg = makeDeg();
-  var quant = $(".slider_box .slider_content").length / 2;
-  var current_number = 1;
-  var rotateEven = 1;
-  $("#next_slide_btn").on("click", function(e) {
-    e.preventDefault();
-    $(".button_box .button_disable").removeClass("no_display");
-    $(".slider_box").rotate(new_deg(), function() {
-      if (current_number == quant) {
-        current_number = 0;
-      }
-      current_number++;
-      rotateEven++;
-      if (rotateEven % 2 == 0) {
-        $(".slider_content[data-num=" + current_number + "]").css({
-          transform: "rotateY(180deg)"
-        });
-      } else {
-        $(".slider_content[data-num=" + current_number + "]").css({
-          transform: "rotateY(360deg)"
-        });
-      }
-      $(".slider_box .yes_display")
-        .addClass("no_display")
-        .removeClass("yes_display");
-      $(".slider_content[data-num=" + current_number + "]")
-        .addClass("yes_display")
-        .removeClass("no_display");
-      $(".slider_box").rotate(new_deg(), function() {
-        $(".button_box .button_disable").addClass("no_display");
-      });
-    });
-  });
-  //console.log(obj_1["box"][1]);
-}; */
+// jQuery.fn.rotate = function(degrees, f) {
+//     var loc_deg=degrees-90;
+//     var steps= 12;
+//     var callback=f ||function () {};
+//     var that=this;
+//     var timer = setInterval(function () {
+//         loc_deg=loc_deg+7.5;
+//         that.css({'transform' : 'rotateY('+ loc_deg +'deg)'});
+//         steps--;
+//         if(steps===0){
+//             clearInterval(timer);
+//             callback();
+//         }
+//     }, 20);
+//     return $(this);
+// };
